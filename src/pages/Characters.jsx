8c5b4bc9b47ab3c98f074627e69a7a688ee169a3
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-06-19 21:49:02 -0300

    se agrego un spinner en characters y comics para mostrar un mensaje mientras se carga la data

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Navbar, Row } from 'react-bootstrap'
+import { Col, Container, Navbar, Row, Spinner } from 'react-bootstrap'
 import Item from '../components/Item'
 import PageNumbers from '../components/PageNumbers'
+import { useMarvel } from '../MarvelContext'
 
 export default function Characters() {
 
   const [characters, setCharacters] = useState([])
+  //const {characters} = useMarvel()
   const [searchedCharacters, setSearchedCharacters] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false)
 
   const itemsPerPage = 6
   const paginas = []
@@ -20,17 +23,20 @@ export default function Characters() {
   const getCharacters = async () => {
     //console.log('URL', URL_COMICS)
     try {
+      setIsLoading(true)
       const res = await fetch(URL_CHARACTERS).then(result => result.json())
       //const onlyCharacters = res.data.results.filter((comic) => Number(comic.issueNumber) > 0)
-      console.log('RESULTADOSSSSS', res.data.results)
+      console.log('DESDE CHARACTER', res.data.results)
       setCharacters(res.data.results)
       setSearchedCharacters(res.data.results.slice(0, 6))
     } catch (e) {
       console.log('error', e)
     }
+    finally {
+      setIsLoading(false)
+    }
 
   }
-
 
   useEffect(() => {
     getCharacters()
@@ -39,36 +45,47 @@ export default function Characters() {
   return (
     <>
 
-      <Container className='mt-5'>
-        <Row>
-          {
-            searchedCharacters.map((character) => {
-              return <Col xs={12} sm={6} lg={4} xl={3} key={character.id} style={{cursor: 'pointer'}} className='d-flex justify-content-center'>
-                <Item item={character} 
-                backgroundColor={'#1e1e1e'} 
-                colorBarra={'#ed1d24'}
-                colorBarra3={'#331e1f'} 
-                colorBarra4={'#521d1f'}
-                colorBarra5={'#851d21'}
-                colorBarra6={'#b91d22'}></Item>
-              </Col>
-            })
-          }
-        </Row>
-      </Container>
-      <div className='d-flex justify-content-center mb-4'>
-        {paginas.map((num, index) => {
-          return <PageNumbers key={index} 
-          num={num} 
-          paginas={paginas} 
-          setSearchedItems={setSearchedCharacters} 
-          items={characters} 
-          itemsPerPage={itemsPerPage} 
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage} />
-        })}
-      </div>
+      {!isLoading ?
+        <>
+          <Container className='mt-5'>
+            <Row>
+              {
+                searchedCharacters.map((character) => {
+                  return <Col xs={12} sm={6} lg={4} xl={3} key={character.id} style={{ cursor: 'pointer' }} className='d-flex justify-content-center'>
+                    <Item item={character}
+                      backgroundColor={'#1e1e1e'}
+                      colorBarra={'#ed1d24'}
+                      colorBarra3={'#331e1f'}
+                      colorBarra4={'#521d1f'}
+                      colorBarra5={'#851d21'}
+                      colorBarra6={'#b91d22'}></Item>
+                  </Col>
+                })
+              }
+            </Row>
+          </Container>
+          <div className='d-flex justify-content-center mb-4'>
+            {paginas.map((num, index) => {
+              return <PageNumbers key={index}
+                num={num}
+                paginas={paginas}
+                setSearchedItems={setSearchedCharacters}
+                items={characters}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage} />
+            })}
+          </div>
+        </>
+        :
+        <div className='spinner'>
+          <Spinner animation="border" variant="danger" />
+          <div className='ms-3'>Cargando...</div>
+        </div>
+      }
 
     </>
+
+
   )
 }
--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Navbar, Row } from 'react-bootstrap'
+import { Col, Container, Navbar, Row, Spinner } from 'react-bootstrap'
 import Item from '../components/Item'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +10,7 @@ export default function Comics() {
   const [comics, setComics] = useState([])
   const [searchedComics, setSearchedComics] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false)
 
   const itemsPerPage = 6
   const paginas = []
@@ -22,6 +23,7 @@ export default function Comics() {
   const getComics = async () => {
     //console.log('URL', URL_COMICS)
     try {
+      setIsLoading(true)
       const res = await fetch(URL_COMICS).then(result => result.json())
 
       console.log('RESULTADO SIN FILTRO', res.data.results)
@@ -31,6 +33,9 @@ export default function Comics() {
       setSearchedComics(onlyComics.slice(0, 6))
     } catch (e) {
       console.log('error', e)
+    }
+    finally {
+      setIsLoading(false)
     }
 
   }
@@ -43,36 +48,46 @@ export default function Comics() {
   return (
     <>
 
-      <Container className='mt-5'>
-        <Row>
-          {
-            searchedComics.map((comic) => {
-              return <Col xs={12} sm={6} lg={4} xl={3} key={comic.id} style={{cursor: 'pointer'}} className='d-flex justify-content-center'>
-                <Item item={comic} 
-                backgroundColor={'#ed1d24'} 
-                colorBarra={'#1e1e1e'} 
-                colorBarra3={'#d81d23'} 
-                colorBarra4={'#b91e23'}
-                colorBarra5={'#851d21'}
-                colorBarra6={'#511d1f'}
-                ></Item>
-              </Col>
-            })
-          }
-        </Row>
-      </Container>
-      <div className='d-flex justify-content-center mb-4'>
-        {paginas.map((num, index) => {
-          return <PageNumbers key={index} 
-          num={num} 
-          paginas={paginas} 
-          setSearchedItems={setSearchedComics} 
-          items={comics} 
-          itemsPerPage={itemsPerPage} 
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage} />
-        })}
-      </div>
+      {!isLoading ?
+
+        <>
+          <Container className='mt-5'>
+            <Row>
+              {
+                searchedComics.map((comic) => {
+                  return <Col xs={12} sm={6} lg={4} xl={3} key={comic.id} style={{ cursor: 'pointer' }} className='d-flex justify-content-center'>
+                    <Item item={comic}
+                      backgroundColor={'#ed1d24'}
+                      colorBarra={'#1e1e1e'}
+                      colorBarra3={'#d81d23'}
+                      colorBarra4={'#b91e23'}
+                      colorBarra5={'#851d21'}
+                      colorBarra6={'#511d1f'}
+                    ></Item>
+                  </Col>
+                })
+              }
+            </Row>
+          </Container>
+          <div className='d-flex justify-content-center mb-4'>
+            {paginas.map((num, index) => {
+              return <PageNumbers key={index}
+                num={num}
+                paginas={paginas}
+                setSearchedItems={setSearchedComics}
+                items={comics}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage} />
+            })}
+          </div>
+        </>
+        :
+        <div className='spinner'>
+          <Spinner animation="border" variant="danger" />
+          <div className='ms-3'>Cargando...</div>
+        </div>
+      }
 
     </>
   )
